25cb754c0172cf90021bc3e4a7f3c2138af5002e
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-09 20:55:23 +0530

    Create SignUp API Route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,10 +12,14 @@ mongoose.connect(process.env.MONGO)
 }).catch((err) => {
     console.log(err);
 });
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server Is Running On Port 3000!!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
