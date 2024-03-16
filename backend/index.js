71d79df8133a31dad6929dd268348e404cbabced
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-16 20:38:40 +0530

    Completed Update User Profile

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,36 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(
-    () => {console.log('Mongo DB is Connected');
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
     console.log(err);
-});
-
+  });
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log('Server Is Running On Port 3000!!');
+  console.log('Server is running on port 3000!');
 });
-
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   currentUser: null,
   error: null,
   loading: false,
 };
-
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -23,9 +21,29 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
