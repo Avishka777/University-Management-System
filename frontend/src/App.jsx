0406c874fe49d64531b648e5d5e0fcf970eeaa74
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-16 20:28:22 +0530

    Make Dashboard Private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header"
 import Programmes from "./pages/Programmes"
 import TimeTable from "./pages/TimeTable"
 import Footer from "./components/Footer"
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -18,7 +19,9 @@ export default function App() {
         <Route path="/about" element={<About />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/sign-in" element={<SignIn />}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path="/programmes" element={<Programmes/>}/>
         <Route path="/time-table" element={<TimeTable/>}/>
 
