99baf01a6789f0a80dd5bd7775ffa86b649d1eb1
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-17 17:40:20 +0530

    Add Delete User Function for Admin

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -51,8 +51,9 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
 export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
       return next(errorHandler(403, 'You are not allowed to delete this user'));
     }
     try {
@@ -72,7 +73,6 @@ export const deleteUser = async (req, res, next) => {
       next(error);
     }
   };
-
   export const getUsers = async (req, res, next) => {
     if (!req.user.isAdmin) {
       return next(errorHandler(403, 'You are not allowed to see all users'));
@@ -81,21 +81,21 @@ export const deleteUser = async (req, res, next) => {
       const startIndex = parseInt(req.query.startIndex) || 0;
       const limit = parseInt(req.query.limit) || 9;
       const sortDirection = req.query.sort === 'asc' ? 1 : -1;
-
+  
       const users = await User.find()
         .sort({ createdAt: sortDirection })
         .skip(startIndex)
         .limit(limit);
-
+  
       const usersWithoutPassword = users.map((user) => {
         const { password, ...rest } = user._doc;
         return rest;
       });
-
+  
       const totalUsers = await User.countDocuments();
-
+  
       const now = new Date();
-
+  
       const oneMonthAgo = new Date(
         now.getFullYear(),
         now.getMonth() - 1,
@@ -104,7 +104,7 @@ export const deleteUser = async (req, res, next) => {
       const lastMonthUsers = await User.countDocuments({
         createdAt: { $gte: oneMonthAgo },
       });
-
+  
       res.status(200).json({
         users: usersWithoutPassword,
         totalUsers,
