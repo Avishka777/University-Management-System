21a2483d5d7ffec3a1ce1d21c0531855bc7ed351
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-17 17:27:54 +0530

    Add Show User Notifications Inside Dashboard

--- frontend/src/components/DashNotifications.jsx
+++ frontend/src/components/DashNotifications.jsx
@@ -1,5 +1,89 @@
+import { Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 export default function DashNotifications() {
-    return (
-      <div>DashNotifications</div>
-    )
-  }
+  const { currentUser } = useSelector((state) => state.user);
+  const [userNotifications, setUserNotifications] = useState([]);
+  console.log(userNotifications);
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const res = await fetch(`/api/notification/getnotifications?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserNotifications(data.notifications);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchNotifications();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && userNotifications.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Notification image</Table.HeadCell>
+              <Table.HeadCell>Notification title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userNotifications.map((notification) => (
+              <Table.Body className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(notification.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/notification/${notification.slug}`}>
+                      <img
+                        src={notification.image}
+                        alt={notification.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/notification/${notification.slug}`}
+                    >
+                      {notification.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{notification.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-notification/${notification._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no notifications yet!</p>
+      )}
+    </div>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+};
