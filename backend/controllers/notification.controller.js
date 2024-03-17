0496f8c67fbc1a6e169ed15a106f5691c2bfaa93
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-17 17:25:49 +0530

    Add Get Notifications API Route

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,6 +1,5 @@
 import Notification from '../models/notification.model.js';
 import { errorHandler } from '../utils/error.js';
-
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to create a notification'));
@@ -21,6 +20,51 @@ export const create = async (req, res, next) => {
   try {
     const savedNotification = await newNotification.save();
     res.status(201).json(savedNotification);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getnotifications = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const notifications = await Notification.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.notificationId && { _id: req.query.notificationId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalNotifications = await Notification.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthNotifications = await Notification.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      notifications,
+      totalNotifications,
+      lastMonthNotifications,
+    });
   } catch (error) {
     next(error);
   }
--- backend/routes/notiication.route.js
+++ backend/routes/notiication.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/notification.controller.js';
+import { create, getnotifications } from '../controllers/notification.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
-
+router.get('/getnotifications', getnotifications)
 
 export default router;
