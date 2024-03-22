f0d2e04c1f09d76b5725fb31e592c19e4f296626
Avishka777 <a.rathnakumara777@gmail.com>
2024-03-22 16:24:14 +0530

    Add Search Function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreateNotification from './pages/CreateNotification';
 import UpdateNotification from './pages/UpdateNotification';
 import NotificationPage from './pages/NotificationPage';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/about" element={<About />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/sign-in" element={<SignIn />}/>
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import NotificationCard from '../components/NotificationCard';
 
 export default function Home() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      const res = await fetch('/api/notification/getNotifications');
+      const data = await res.json();
+      setNotifications(data.notifications);
+    };
+    fetchNotifications();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl'>The Future</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+        We are a leading non-state higher education institute approved by the University Grants Commission (UGC) under the Universities Act. 
+        We are members of the Association of Commonwealth Universities (ACU), as well as the International Association of Universities (IAU). 
+        We are also the first Sri Lankan institute to be accredited by the Institute of Engineering & Technology( IET), UK and Engineering Council, UK.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          View all Notifications
+        </Link>
+      </div>
+
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {notifications && notifications.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Notifications</h2>
+            <div className='flex flex-wrap gap-4'>
+              {notifications.map((notification) => (
+                <NotificationCard key={notification._id} notification={notification} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all Notifications
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
